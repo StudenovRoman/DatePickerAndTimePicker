1c95717409dcc7f717cbe48e1c61dd46d2a101a4
Studenov <rstudenov@allmax.team>
2018-11-26 14:21:46 +0700

    update logic timepicker

--- imports/components/helpers/checkTime.js
+++ imports/components/helpers/checkTime.js
@@ -9,20 +9,23 @@ export const checkStart = (onChange, time, end) => {
   const sh = moment(time, 'HH:mm:ss').hour();
   const eh = moment(end, 'HH:mm:ss').hour();
   let newStart = null;
-  if (sh === eh && sm === em && (ss === es || ss >= es)) {
+  if (sh === eh && sm === em && ss === es) {
     newStart = moment(time).second(es - 1);
+    return onChange({ start: moment(newStart).toDate(), end });
   }
-  if (sh === eh && sm >= em && (ss === es || ss >= es)) {
+  if (sh === eh && sm === em && ss >= es) {
     newStart = moment(time).minute(em - 1);
+    return onChange({ start: moment(newStart).toDate(), end });
   }
   if (eh === 0 && em === 0 && es === 0) {
     newStart = moment().hour(0).minute(0).second(0);
+    return onChange({ start: moment(newStart).toDate(), end });
   }
-  if (sh >= eh) {
-    newStart = moment(time).hour(eh + 1);
+  if (sh > eh) {
+    newStart = moment(time).hour(eh).minute(em - 1);
+    return onChange({ start: moment(newStart).toDate(), end });
   }
-  const newTime = newStart ? newStart : time;
-  return onChange({ start: moment(newTime).toDate(), end });
+  return onChange({ start: moment(time).toDate(), end });
 };
 
 export const checkEnd = (onChange, time, start) => {
@@ -34,15 +37,17 @@ export const checkEnd = (onChange, time, start) => {
   const sh = moment(start, 'HH:mm:ss').hour();
   const eh = moment(time, 'HH:mm:ss').hour();
   let newEnd = null;
-  if (sh === eh && sm === em && (ss === es || ss >= es)) {
+  if (sh === eh && sm === em && ss === es) {
     newEnd = moment(time).second(ss + 1);
+    return onChange({ start, end: moment(newEnd).toDate() });
   }
-  if (sh === eh && sm >= em && (ss === es || ss >= es)) {
+  if (sh === eh && sm === em && ss >= es) {
     newEnd = moment(time).minute(sm + 1);
+    return onChange({ start, end: moment(newEnd).toDate() });
   }
-  if (sh >= eh) {
-    newEnd = moment(time).hour(sh + 1);
+  if (sh > eh) {
+    newEnd = moment(time).hour(sh).minute(sm + 1);
+    return onChange({ start, end: moment(newEnd).toDate() });
   }
-  const newTime = newEnd ? newEnd : time;
-  return onChange({ start, end: moment(newTime).toDate() });
+  return onChange({ start, end: moment(time).toDate() });
 };
